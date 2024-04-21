e4e229c9a9e6a29ece8afe9c77aa69c5e072b5cb
Alexsander Hage de Melo <hagemelo@gmail.com>
2024-04-21 15:50:19 -0300

    feat: install typeorm

---
+++ backend-schedule-registration/src/config/database/config.ts
@@ -0,0 +1,27 @@
+import { config } from 'dotenv';
+
+import { join } from 'path';
+import { DataSource } from 'typeorm';
+
+config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
+
+const dbConfig: DataSource = new DataSource({
+  type: 'postgres',
+  entities: [
+    join(
+      __dirname,
+      '../../../**/src/features/**/infrastructure/storage/entities/*.entity.{ts,js}',
+    ),
+    join(
+      __dirname,
+      '../../../../libs/**/src/infrastructure/storage/entities/*.entity.{ts,js}',
+    ),
+  ],
+  migrations: [join(__dirname, '/migrations/*.{ts,js}')],
+  url: process.env.DATABASE_URL,
+  logging: false,
+  synchronize: false,
+  port: 5432,
+});
+
+export default dbConfig;
